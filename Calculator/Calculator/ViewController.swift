f7868b4670607c82ef16ece958c981936db5ee1a
Vidya Rangasayee <vidya@hunt.com>
2016-10-20 10:52:55 -0700

    Deleting unneeded files

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Narayan Balasubramanian on 10/20/16.
+//  Copyright © 2016 SJSU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
